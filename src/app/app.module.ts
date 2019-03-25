b289f88fcb86a21a9562325a08e387375d71fc5e
Tomasz Lichon <xorver@gmail.com>
2019-03-25 19:22:32 +0100

    Reactive forms part 1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import {MatFormFieldModule} from '@angular/material';
 import {MatInputModule} from '@angular/material';
 import {MatCheckboxModule} from '@angular/material';
 import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material';
+import {MatSlideToggleModule} from '@angular/material';
 
 import 'hammerjs';
 import {MenuComponent} from './menu/menu.component';
@@ -57,7 +60,10 @@ import {LoginComponent} from './login/login.component';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [
     DishService,
---
+++ src/app/shared/feedback.ts
@@ -0,0 +1,11 @@
+export class Feedback {
+  firstname: string;
+  lastname: string;
+  telnum: number;
+  email: string;
+  afree: boolean;
+  contacttype: string;
+  message: string;
+}
+
+export const ContactType = ['None', 'Tel', 'Email'];
