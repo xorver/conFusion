52114a56a332e8b2828c8f0cefc0ae915e3ac095
Tomasz Lichon <xorver@gmail.com>
2019-05-24 13:37:30 +0200

    Directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import {LoginComponent} from './login/login.component';
 import {HttpClientModule} from '@angular/common/http';
 import {baseURL} from './shared/baseurl';
 import {ProcessHTTPMsgService} from './services/process-httpmsg.service';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,8 @@ import {ProcessHTTPMsgService} from './services/process-httpmsg.service';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
